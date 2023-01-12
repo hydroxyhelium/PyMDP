fde72261e4e28c303b571c30f941bb7a8b86160a
Priyanshu Sharma <sharmapriyanshu119@gmail.com>
2023-01-11 22:05:39 -0500

    added the code for reward function

--- reward.py
+++ reward.py
@@ -1,8 +1,12 @@
 ## Reward function will be wrapped with this class
 
-
 class Reward:
     def __init__(self, rf_dict:dict)->None:
         self.rf_dict=rf_dict
         return
+
+    def get_reward(self, state:str, action;str)->int:
+        temp_tuple = (state, action)
+        return self.rf_dict[temp_tuple]
+        
     
