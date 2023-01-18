48e97ea31042a6a3da02998b25d65659e1f9708f
Priyanshu Sharma <sharmapriyanshu119@gmail.com>
2023-01-17 21:39:00 -0500

    added a transformer class for actions

--- spark_pipeline.py
+++ spark_pipeline.py
@@ -64,5 +64,38 @@ class StateTransformer(Transformer, HasInputCol, HasOutputCol, DefaultParamsRead
 
     return df.withColumn(output_col, convert_states(input_col))
 
+class ActionTransformer(Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable):
+  """
+    Custom Transformer wrapper class for convert_states()
+  """ 
+
+  input_col = Param(Params._dummy(), "input_col", "input column name.", typeConverter=TypeConverters.toString)
+  output_col = Param(Params._dummy(), "output_col", "output column name.", typeConverter=TypeConverters.toString)
+
+  @keyword_only
+  def __init__(self, input_col: str = "input", output_col: str = "output"):
+    super(StateTransformer, self).__init__()
+    self._setDefault(input_col=None, output_col=None)
+    kwargs = self._input_kwargs
+    self.set_params(**kwargs)
+
+  @keyword_only
+  def set_params(self, input_col: str = "input", output_col: str = "output"):
+    kwargs = self._input_kwargs
+    self._set(**kwargs)
+  
+  def get_input_col(self):
+    return self.getOrDefault(self.input_col)
+
+  def get_output_col(self):
+    return self.getOrDefault(self.output_col)
+
+  def _transform(self, df: DataFrame):
+    input_col = self.get_input_col()
+    output_col = self.get_output_col()
+
+    return df.withColumn(output_col, convert_actions(input_col))
+
+
 
 
