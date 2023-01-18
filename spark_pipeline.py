5ccf2ac46ef0e7bc810c5d3eabf17c96c326c51c
Priyanshu Sharma <sharmapriyanshu119@gmail.com>
2023-01-17 21:37:37 -0500

    update: made a custom transformer class for state transformer

--- spark_pipeline.py
+++ spark_pipeline.py
@@ -3,6 +3,13 @@
 import pyspark
 import pandas as pd
 from pyspark.sql import SparkSession
+from pyspark import keyword_only  # Note: use pyspark.ml.util.keyword_only if Spark < 2.0
+from pyspark.ml import Transformer
+from pyspark.ml.param.shared import HasInputCol, HasOutputCol, Param, Params, TypeConverters
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
 
 spark = SparkSession.builder.appName('Test').getOrCreate()
 
@@ -14,9 +21,6 @@ df_spark_actions.show()
 df_spark_states = spark.read.option('header', 'true').csv('states.csv')
 df_spark_states.show() 
 
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType
-
 @udf(returnType=StringType())
 def convert_states(s):
   return str(s)
@@ -25,19 +29,40 @@ def convert_states(s):
 def convert_actions(s):
   return str(s)
 
-df_spark_states.select(convert_states("States")).show()
-
-
-from pyspark import keyword_only  # Note: use pyspark.ml.util.keyword_only if Spark < 2.0
-from pyspark.ml import Transformer
-from pyspark.ml.param.shared import HasInputCol, HasOutputCol, Param, Params, TypeConverters
-from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+# df_spark_states.select(convert_states("States")).show()
 
 
 class StateTransformer(Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable):
   """
     Custom Transformer wrapper class for convert_states()
   """ 
+
+  input_col = Param(Params._dummy(), "input_col", "input column name.", typeConverter=TypeConverters.toString)
+  output_col = Param(Params._dummy(), "output_col", "output column name.", typeConverter=TypeConverters.toString)
+
+  @keyword_only
+  def __init__(self, input_col: str = "input", output_col: str = "output"):
+    super(StateTransformer, self).__init__()
+    self._setDefault(input_col=None, output_col=None)
+    kwargs = self._input_kwargs
+    self.set_params(**kwargs)
+
+  @keyword_only
+  def set_params(self, input_col: str = "input", output_col: str = "output"):
+    kwargs = self._input_kwargs
+    self._set(**kwargs)
   
+  def get_input_col(self):
+    return self.getOrDefault(self.input_col)
+
+  def get_output_col(self):
+    return self.getOrDefault(self.output_col)
+
+  def _transform(self, df: DataFrame):
+    input_col = self.get_input_col()
+    output_col = self.get_output_col()
+
+    return df.withColumn(output_col, convert_states(input_col))
+
 
 
